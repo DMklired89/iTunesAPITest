da860a763df6fda67437cbf69c986cfe22968eef
Dmitry Edrenov <edrenoff@icloud.com>
2018-05-12 22:47:53 +0600

    Initial Commit

---
+++ iTunesTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iTunesTest
+//
+//  Created by Dmitry Edrenov on 12.05.18.
+//  Copyright © 2018 Dmitry Edrenov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iTunesTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iTunesTest
+//
+//  Created by Dmitry Edrenov on 12.05.18.
+//  Copyright © 2018 Dmitry Edrenov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
